126832db696cd4f9de3dae4e0a7561a629187984
Tomas Hofman <thofman@redhat.com>
2018-09-04 15:18:01 +0200

    initial commit

---
+++ src/main/java/com/redhat/thofman/jpa/MyBean.java
@@ -0,0 +1,45 @@
+package com.redhat.thofman.jpa;
+
+import java.io.PrintWriter;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.annotation.Resource;
+import javax.ejb.Stateless;
+import javax.sql.DataSource;
+import javax.transaction.Transactional;
+
+@Stateless
+@Transactional
+public class MyBean {
+
+    private final static Logger LOG = Logger.getLogger(MyBean.class.getName());
+
+    private final static int ITERATIONS = 10;
+
+    @Resource
+    private DataSource dataSource;
+
+    public void beanMethod(PrintWriter writer) {
+        Connection connection = null;
+        try {
+            connection = dataSource.getConnection();
+
+            for (int i = 0; i < ITERATIONS; i++) {
+                PreparedStatement preparedStatement = connection.prepareStatement("select 1");
+
+                ResultSet resultSet = preparedStatement.executeQuery();
+                resultSet.next();
+                int intCol = resultSet.getInt(1);
+                LOG.info("Result: " + intCol);
+                writer.println("Result: " + intCol);
+                Thread.sleep(200);
+            }
+        } catch (Exception e) {
+            LOG.log(Level.SEVERE, "Statement failed", e);
+        }
+    }
+
+}
---
+++ src/main/java/com/redhat/thofman/jpa/MyServlet.java
@@ -0,0 +1,69 @@
+package com.redhat.thofman.jpa;
+
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import javax.annotation.Resource;
+import javax.inject.Inject;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.sql.DataSource;
+import javax.transaction.SystemException;
+import javax.transaction.UserTransaction;
+
+@WebServlet("/MyServlet")
+public class MyServlet extends HttpServlet {
+
+    private final static int ITERATIONS = 10;
+
+    @Resource
+    private DataSource dataSource;
+
+    @Resource
+    private UserTransaction userTransaction;
+
+    @Inject
+    private MyBean bean;
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.setContentType("text/plain");
+
+        resp.getWriter().println("Executing SQL statement from bean.");
+        bean.beanMethod(resp.getWriter());
+
+        resp.getWriter().println("Executing SQL statement from servlet.");
+
+        Connection connection = null;
+        try {
+            userTransaction.begin();
+
+            connection = dataSource.getConnection();
+
+            for (int i = 0; i < ITERATIONS; i++) {
+                PreparedStatement preparedStatement = connection.prepareStatement("select 1");
+
+                ResultSet resultSet = preparedStatement.executeQuery();
+                resultSet.next();
+                int intCol = resultSet.getInt(1);
+                resp.getWriter().println("Result: " + intCol);
+                Thread.sleep(200);
+            }
+
+            userTransaction.commit();
+        } catch (Exception e) {
+            try {
+                userTransaction.rollback();
+            } catch (SystemException e1) {
+                e1.printStackTrace();
+            }
+            throw new ServletException(e);
+        }
+
+        resp.getWriter().close();
+    }
+}
